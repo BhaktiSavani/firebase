405a7f7b818e38c8460b0ca06e99e2cb4cec4341
BhaktiSavani <bhakti.savani33@gmail.com>
2019-06-17 16:13:21 -0400

    3

---
+++ app/src/main/java/com/example/a1894423/firebase/Github.java
@@ -0,0 +1,42 @@
+package com.example.a1894423.firebase;
+
+public class Github {
+
+    int id;
+    int watchers;
+    String full_name;
+
+
+    public int getWatchers() {
+        return watchers;
+    }
+
+    public void setWatchers(int watchers) {
+        this.watchers = watchers;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/a1894423/firebase/MainActivity.java
+++ app/src/main/java/com/example/a1894423/firebase/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.a1894423.firebase;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtname;
+    ArrayList<Github> vals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtname =findViewById(R.id.txtname);
+        vals =new ArrayList<>();
+
         FirebaseApp.initializeApp(this);
+         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("items");
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-        myRef.setValue("Hello, Worlds!");
+                String nm =dataSnapshot.child("0").child("name").getValue(String.class);
+                txtname.setText(nm);
+
+                Iterable<DataSnapshot> childs= dataSnapshot.getChildren();
+                for (DataSnapshot snap :childs)
+                {
+                   // System.out.println("name :" +snap.child("name").getValue().toString());
+                    Github git =snap.getValue(Github.class);
+                    System.out.println(git.getId()+" "+git.watchers+" "+git.full_name);
+                }
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
